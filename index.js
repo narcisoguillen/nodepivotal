1e66ef1e0cc17852976fa5c517d2d415fb018e58
Narciso Guillen <narciso.guillen@tangosource.com>
2013-06-12 13:53:56 -0500

    Export modules to make things easier

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 //Modules
-var querystring = require('querystring');
 var xml2js      = require('xml2js');
-var https       = require('https');
 var inspect     = require('eyes').inspector({ stream: null });
-
 var parser      = new xml2js.Parser();
-var noAccess    = new RegExp(/Access denied/);
+
+//Configuration file
 var config      = require('./config');
 
 var pivotalnode = {
+
+  //Token
   token: null,
+
+  //Request optins
   requestOptions: function(options){
     return {
       host: 'www.pivotaltracker.com',
       port: config.PORT || 443,
       method: 'GET'
     };
-  }
+  },
+
+  //Local Modules
+  querystring: require('querystring'),
+  https:       require('https')
 };
 
-pivotalnode.responseParser = function(callb){
+pivotalnode.__responseParser = function(callb){
   return function(res){
     res.on('data', function(data){
       parser.parseString(data, function (error, result) {
@@ -29,84 +35,13 @@ pivotalnode.responseParser = function(callb){
   };
 };
 
-pivotalnode.getToken = function(data, cb){
-  var callback = typeof cb === 'function' ? cb : function(){ };
-  var options  = new this.requestOptions();
-
-  if(!data || typeof data !== 'object'){ return callback('Missing credentials'); }
-
-  var stringData = querystring.stringify(data);
-
-  options.path    = '/services/v3/tokens/active';
-  options.method  = 'POST';
-  options.headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',  
-      'Content-Length': stringData.length
-  };
-
-  var request = https.request(options, new this.responseParser(function(error, result){
-    pivotalnode.token = result ? result.token.guid[0] : null;
-    return callback(error, result);
-  }));
-
-  request.write(stringData);
-  request.end();
-
-  request.on('error', function(error){
-    return callback(error);
-  });
-
+pivotalnode.set = function(name){
+  pivotalnode[name] = require('./lib/'+name);
 };
 
-pivotalnode.activityFeed = function(data, cb){
-  var callback = typeof data === 'function' ? data : typeof cb === 'function' ? cb : function(){ };
-  var options  = new this.requestOptions();
-  var project;
-
-  if(!this.token){ return callback('Token is missing'); }
-
-  if(data.project){
-    project = data.project; delete data.project; 
-  }
-
-  var stringData = querystring.stringify(data);
-  var params     = stringData ? ('?' + stringData) : '';
-
-  options.headers = { 'X-TrackerToken': this.token };
-  options.path    = project ? '/services/v3/projects/'+project+'/activities' : '/services/v3/activities';
-  options.path+=params;
-
-  var request = https.request(options, new this.responseParser(function(error, result){
-    return callback(error, result);
-  }));
-
-  request.end();
-
-  request.on('error', function(error){
-    return callback(error);
-  });
-
-};
-
-pivotalnode.getProject = function(data, cb){
-  var callback = typeof data === 'function' ? data : typeof cb === 'function' ? cb : function(){ };
-  var options  = new this.requestOptions();
-
-  if(!this.token){ return callback('Token is missing'); }
-
-  options.path    = '/services/v3/projects';
-  options.headers = { 'X-TrackerToken': this.token };
-  options.path+= data.project ? '/' + data.project : '';
-
-  var request = https.request(options, new this.responseParser(function(error, result){
-    return callback(error, result);
-  }));
-
-  request.end();
-
-  request.on('error', function(error){
-    return callback(error);
-  });
-};
+//Public methods
+pivotalnode.set('getToken');
+pivotalnode.set('activityFeed');
+pivotalnode.set('getProject');
 
 module.exports = pivotalnode;
